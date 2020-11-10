a80e18837b429b533c8817ebac3a7d70bf995db2
Ajay-Chidambaram <56651112+Ajay-Chidambaram@users.noreply.github.com>
2020-11-10 22:34:56 +0530

    Create server.js

---
+++ server/server.js
@@ -0,0 +1,29 @@
+const http = require('http');
+const express = require('express');
+const socketio = require('socket.io');
+
+const app = express();
+
+const clientPath = `${__dirname}/../client`;
+app.use(express.static(clientPath));
+
+const server = http.createServer(app);
+const io = socketio(server);
+
+const PORT = 8080 || process.env.PORT;
+
+io.on('connection', (sock) => {
+      sock.emit('message', "Hi you are connected");
+
+      sock.on('message', (text) => {
+            io.emit('message', text);
+      });
+});
+
+server.on('error', (err) => {
+      console.log("Error Occured ", err);
+});
+
+server.listen(PORT, () => {
+      console.log(`Chat Box started on ${PORT}`);
+});
